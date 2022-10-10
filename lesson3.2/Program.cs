9943b7d48368c3d2b967c685fd66ea44002092a4
Ivan Chevin <chevin.gm@gmail.com>
2022-10-10 21:40:16 +0300

    конец лекции 3.2

--- lesson3.2/Program.cs
+++ lesson3.2/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*int Max(int arg1, int arg2, int arg3);
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result
+}*/
+
+// ТИП ДАННЫХ[] ИМЯ = {Значение1,Значение2, ...}
+// int[] array = { 9, 28, 1, 32, 1990, 8 }
+
+// int Max(int arg1, int arg2, int arg3);
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result
+// }
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count) ;
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
