90733430571270f9739e0f8effe7baf759c9173c
Ivan Chevin <chevin.gm@gmail.com>
2022-10-10 12:14:39 +0300

    семинар 3.1

---
+++ sem3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу которая на вход принимает число и выдает его квадрат
+int a, b;
+string userEnter;
+
+Console.Write("Введите число: ");
+userEnter = Console.ReadLine()!;
+
+//a = int.Parse(userEnter);
+int.TryParse(userEnter, out a);
+
+b = a * a;
+
+Console.Write($"Квадрат введенного числа = {b}\n");// $ - интерполяция
+
+if (b > 100)
+{
+    Console.WriteLine("Получилось!");
+}
+else
+{
+    Console.WriteLine("Не получилось");
+}
+
+int i = 0;
+while (i < 10) // true false
+{
+    Console.Write($"{i + 1} ");
+    i = i + 1;// i++
+}
