70cf1adb290451c9d597921f9e4046d3989d4fbe
Ivan Chevin <chevin.gm@gmail.com>
2022-10-11 20:34:26 +0300

    решил задачу 23

--- hw3.3/Program.cs
+++ hw3.3/Program.cs
@@ -24,6 +24,7 @@ else
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+/*
 int[] A = new int[3];
 int[] B = new int[3];
 
@@ -41,3 +42,19 @@ Console.Write($"Введите координату Z2:");
 int.TryParse(Console.ReadLine()!, out B[2]);
 double c = Math.Sqrt((A[0] - B[0]) * (A[0] - B[0]) + (A[1] - B[1]) * (A[2] - B[2]) + (A[2] - B[2]) * (A[2] - B[2]));
 Console.Write($"расстояние между точками = {c}");
+*/
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int N = 0;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+for (int i = 1; i <= N; i++)
+    Console.Write($"{i * i * i} ");
+
+
+
