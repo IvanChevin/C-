59edab23c2702628681933d59d4421d1f33e69f4
Ivan Chevin <chevin.gm@gmail.com>
2022-10-11 19:52:59 +0300

    решил задачу 19

--- hw3.3/Program.cs
+++ hw3.3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int a, b, c, d, userEnter;
+int.TryParse(Console.ReadLine()!, out userEnter);
+a = userEnter / 10000;
+b = userEnter / 1000 % 10;
+c = userEnter / 10 % 10;
+d = userEnter % 10;
+if (a == d && b == c)
+    Console.Write($"да");
+else
+    Console.Write($"нет");
