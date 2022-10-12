03c8fec6356031bdebec4392f1bc33a58d4ebd18
Ivan Chevin <chevin.gm@gmail.com>
2022-10-12 15:33:41 +0300

    решил задачи 25 27 29

--- hw3.4/Program.cs
+++ hw3.4/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите функцию, используя цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+/*    int a, b;
+Console.Write($"введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+Console.Write($"{stepen(a, b)}");
+
+int stepen(int _a, int _b)
+{
+    int proizv = 1;
+    for (int i = 1; i <= _b; i++)
+        proizv *= _a;
+    return proizv;
+}       */
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+/*  Console.Write($"введите число: ");
+string a = Console.ReadLine()!;
+Console.Write($"{Sum(a)}");
+
+int Sum(string _a)
+{
+    int _sum = 0;
+    int _len = _a.Length;
+    int.TryParse(_a, out int _znachenie);
+    for (int i = 0; i < _len; i++)
+    {
+        _sum = _sum + _znachenie % 10;
+        _znachenie = _znachenie / 10;
+    }
+    return _sum;
+}   */
+
+// Задача 29: Напишите функцию, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+FillArray(array);
+
+void FillArray(int[] _array)
+{
+    int len = _array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        _array[i] = new Random().Next(100);
+        Console.Write($"{_array[i]} ");
+    }
+}
--- sem3.4/Program.cs
+++ sem3.4/Program.cs
@@ -1,2 +1,170 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36 
+
+/*
+int a = 0;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+int sum = 0;
+for (int i = 1; i <= a; i++)
+sum += i;
+
+Console.Write($"Сумма чисел от 1 до {a} = {sum}");
+*/
+/*
+int a = 0;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Console.Write($"Сумма чисел от 1 до {a} = {Sum(a)}");
+
+int Sum(int _a) // метод (функция)
+{
+    int sum = 0;
+    for (int i = 1; i <= _a; i++)
+        sum += i;
+    return sum;
+}
+*/
+
+
+// Чтото про типы данных..
+
+// int Max(int a, int b)
+// {
+// if (a > b)
+// return a;
+// return b;
+// }
+
+// double Div(int a, int b)
+// {
+// return (double)a / (double)b;
+// }
+
+// string Output(string text)
+// {
+// return $"+{text}+";
+// }
+
+// bool IsEven(int a)
+// {
+// if (a % 2 == 0)
+// return true;
+// return false;
+// }
+
+// int[] CreateArray(int size)
+// {
+// int[] _array = new int[size];
+// return _array;
+// }
+
+// void print(string _text)
+// {
+// Console.Write(_text);
+// return;
+// }
+
+// print($"Это использование функции void");
+
+// Чтото про глобальные переменные
+/*
+int G_i = 0;
+string G_str = "";
+
+Max(5);
+
+Console.Write($"{G_i} {G_str}");
+
+void Max(int a)
+{
+G_i = a;
+if (a > 0)
+G_str = "переменная положительна";
+else if (a < 0)
+G_str = "переменная отрицательна";
+else
+G_str = "переменная равна 0";
+Min(a);
+}
+
+void Min(int a)
+{
+G_i *= -1;
+}
+*/
+
+// Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+/*
+int N = 0;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+Console.Write($"Произведение чисел от 1 до {N} = {Factorial(N)}");
+
+int Factorial(int _N)
+{
+int factorial = 1;
+for (int i = 1; i <= _N; i++)
+factorial *= i;
+return factorial;
+}
+*/
+
+// Напишите функцию, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+/*
+int[] array;
+
+array = CreateArray();
+for (int i = 0; i < array.Length; i++)
+Console.Write($"{array[i]} ");
+
+int[] CreateArray()
+{
+int[] _array = new int[8];
+for (int i = 0; i < 8; i++)
+_array[i] = new Random().Next(0, 2);
+return _array;
+}
+*/
+
+/*
+int a = 5;
+
+Console.WriteLine(a);
+
+Multiply(ref a);
+
+Console.WriteLine(a);
+
+void Multiply(ref int _a)
+{
+_a *= 2;
+}
+*/
+
+//пример использования foreach
+/*
+int[] array = new int[20];
+
+foreach (var item in CreateArray(20))
+Console.Write($"{item} ");
+
+int[] CreateArray(int _size)
+{
+int[] _array = new int[_size];
+for (int i = 0; i < _size; i++)
+_array[i] = new Random().Next(10);
+return _array;
+}
+*/
