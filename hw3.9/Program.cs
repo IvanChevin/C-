c53834791e003d2722ac880bd3c28dea51b959c1
Ivan Chevin <chevin.gm@gmail.com>
2022-10-29 12:53:30 +0300

    задачи 64 и 68

--- hw3.9/Program.cs
+++ hw3.9/Program.cs
@@ -35,3 +35,16 @@ int RecSum(int _M, int _N)
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+int m = 2;//new Random().Next(1, 10);
+int n = 3;//new Random().Next(1, 10);
+
+int AckermannFunction(int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+    return AckermannFunction(numberM, numberN);
+}
+
+Console.Write($"m = {m}, n = {n} -> {AckermannFunction(m, n)}");
